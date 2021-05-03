3730454bbb93b7203fc57c3f20b898809b0e2393
jakecolder93 <jakecolder93@gmail.com>
2021-05-03 20:28:27 +0100

    Basic user entry form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-const App = () => <div>Hello, world.</div>;
+/* eslint-disable prettier/prettier */
+const App = () => {
+  return(
+  <form>
+        <h1>Hello there!</h1>
+        <p>Please enter your 4-digit account reference number:</p>
+        <input
+          type="text"
+          placeholder="e.g. 1001"
+        />
+        <button>Submit</button>
+      </form>
+  )}
 
 export default App;
