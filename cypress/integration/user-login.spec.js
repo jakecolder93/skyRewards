22f5380d6a9a580d9ccea783d3dedf35bf13e137
jakecolder93 <jakecolder93@gmail.com>
2021-05-04 09:36:13 +0100

    E2E test for form submission

--- cypress/integration/user-login.spec.js
+++ cypress/integration/user-login.spec.js
@@ -9,4 +9,16 @@ describe('User form', () => {
             cy.get('p').contains('Please enter your 4-digit account reference number:')
         })
     })
+    it('Does not submit without a user input', () => {
+        cy.visit('/')
+
+        cy.get('#formVal').invoke('submit', (e) => {
+            e.preventDefault()
+            throw new Error('Submitting without value!!!')
+        })
+
+        cy.get('#formVal').within(() => {
+            cy.get('button').click()
+        })
+    })
 })
